9859aacdab8270d4f0756c638f923f991bf21467
Ohk <he@mare5.com>
2020-09-23 13:15:55 +0900

    addItemsToNav function implement

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,16 +17,22 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll(".landing__container");
+const navbarList = document.querySelector("#navbar__list")
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
-
+function addItemsToNav(sections, navbarList){
+    for(const section of sections) {
+        const liElement = document.createElement("li");
+        liElement.className = "menu__link";
+        liElement.textContent = section.querySelector("h2").textContent;
+        navbarList.appendChild(liElement);
+    }
+}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -34,6 +40,7 @@
 */
 
 // build the nav
+    addItemsToNav(sections,navbarList);
 
 
 // Add class 'active' to section when near top of viewport
